c86ae9445fa14ccad48a93909076bbbbc978f539
jnakagawa1 <45570346+jnakagawa1@users.noreply.github.com>
2021-01-31 12:10:58 -0300

    Alterações e correções da página inicial (#1)
    * Update storybook dependencies
    
    * Update home page
    
    * Change to version 0.1.1

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,20 @@
 module.exports = {
   stories: [
-    "../src/stories/**/*.stories.(js|mdx)",
-    "../src/components/**/*.stories.(js|mdx)",
-    "../src/styles/**/*.stories.(js|mdx)",
+    "../src/stories/**/*.stories.@(js|mdx)",
+    "../src/components/**/*.stories.@(js|mdx)",
+    "../src/styles/**/*.stories.@(js|mdx)",
   ],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "@storybook/addon-viewport",
-    //"@storybook/addon-docs",
     {
       name: "@storybook/addon-docs",
       options: {
         configureJSX: true,
       },
     },
+    "@storybook/addon-essentials",
     "@storybook/addon-knobs",
   ],
 };

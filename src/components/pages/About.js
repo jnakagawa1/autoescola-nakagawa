f169e26939ea6e27074d4f21f2151602cc5bd312
jnakagawa1 <jeannak@gmail.com>
2021-03-20 20:37:30 -0300

    Create routes directory

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,8 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
 import styled from "styled-components";
-
-import { useScrollToTop } from "hooks/scroll";
 
 import Hero from "components/molecules/Hero";
 import Heading from "components/atoms/Heading";
@@ -63,108 +60,100 @@ const instructors = [
   },
 ];
 
-const About = () => {
-  useScrollToTop();
-
-  return (
-    <>
-      <Hero image={HeroImage}>
-        <Heading>
-          <h1>Auto Escola Nakagawa</h1>
-        </Heading>
-        <BreadCrumb
-          items={[{ label: "Início", link: "/" }, { label: "Sobre" }]}
-        />
-      </Hero>
-      <Section>
-        <Grid md={2}>
-          <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-              nemo laudantium aspernatur harum amet, modi quidem dolorem magni
-              vel beatae asperiores rerum ab quos nisi laborum reiciendis
-              molestias odio. Quaerat!
-            </p>
-            <p>
-              Fugiat alias voluptatibus aspernatur rem eos aut molestias saepe
-              corporis, perspiciatis quod minima, eveniet facere consectetur
-              odio sed voluptates, maxime incidunt magni deleniti illo obcaecati
-              reprehenderit ipsa! Illum, non tempore.
-            </p>
-            <p>
-              Velit sunt beatae quasi quibusdam voluptates fugiat sequi
-              reprehenderit id, quos, quod fuga. Minus corporis tenetur ipsum,
-              ullam iure odit quibusdam cumque voluptatem repellendus. Adipisci
-              aliquam sunt minima magni voluptatibus?
-            </p>
-            <ul>
-              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
-              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
-              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
-              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
-              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
-            </ul>
-          </div>
-          <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
-              doloremque? Omnis officiis aliquam eum, reprehenderit
-              necessitatibus earum deserunt sapiente porro dolore possimus eaque
-              ut dolores harum facilis! Laboriosam, harum libero?
-            </p>
-            <center>
-              <ImageContainer>
-                <AboutImage />
-              </ImageContainer>
-            </center>
-          </div>
-        </Grid>
-      </Section>
-      <Section inverse>
-        <Grid md={2}>
-          <div>
-            <h4>Missão</h4>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
-              reiciendis ipsam eius vel, temporibus nobis assumenda corporis
-              optio, ratione debitis quia, culpa sunt? Excepturi iste rerum hic
-              natus perspiciatis cupiditate?
-            </p>
-          </div>
-          <div>
-            <h4>Visão</h4>
-            <p>
-              Quas nostrum repellendus, autem corporis expedita debitis ea
-              eveniet consectetur? Nesciunt voluptatum in est reiciendis
-              blanditiis nam totam dolorem et ratione labore. Suscipit maxime
-              libero placeat adipisci eveniet. Laborum, perspiciatis!
-            </p>
-          </div>
-        </Grid>
-      </Section>
-      <Section>
-        <Heading>
-          <h2>Conheça nossos professores</h2>
-        </Heading>
-        <Grid sm={2} md={3} lg={4}>
-          {instructors.map((instructor) => (
-            <Card key={instructor.id}>
-              <CardMedia image={instructor.avatar}>
-                <CardMediaDescription>
-                  <h5>{instructor.name}</h5>
-                </CardMediaDescription>
-              </CardMedia>
-            </Card>
-          ))}
-        </Grid>
-      </Section>
-      <Footer />
-    </>
-  );
-};
-
-About.defaultProps = {};
-
-About.propTypes = {};
+const About = () => (
+  <>
+    <Hero image={HeroImage}>
+      <Heading>
+        <h1>Auto Escola Nakagawa</h1>
+      </Heading>
+      <BreadCrumb
+        items={[{ label: "Início", link: "/" }, { label: "Sobre" }]}
+      />
+    </Hero>
+    <Section>
+      <Grid md={2}>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            nemo laudantium aspernatur harum amet, modi quidem dolorem magni vel
+            beatae asperiores rerum ab quos nisi laborum reiciendis molestias
+            odio. Quaerat!
+          </p>
+          <p>
+            Fugiat alias voluptatibus aspernatur rem eos aut molestias saepe
+            corporis, perspiciatis quod minima, eveniet facere consectetur odio
+            sed voluptates, maxime incidunt magni deleniti illo obcaecati
+            reprehenderit ipsa! Illum, non tempore.
+          </p>
+          <p>
+            Velit sunt beatae quasi quibusdam voluptates fugiat sequi
+            reprehenderit id, quos, quod fuga. Minus corporis tenetur ipsum,
+            ullam iure odit quibusdam cumque voluptatem repellendus. Adipisci
+            aliquam sunt minima magni voluptatibus?
+          </p>
+          <ul>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
+          </ul>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+            doloremque? Omnis officiis aliquam eum, reprehenderit necessitatibus
+            earum deserunt sapiente porro dolore possimus eaque ut dolores harum
+            facilis! Laboriosam, harum libero?
+          </p>
+          <center>
+            <ImageContainer>
+              <AboutImage />
+            </ImageContainer>
+          </center>
+        </div>
+      </Grid>
+    </Section>
+    <Section inverse>
+      <Grid md={2}>
+        <div>
+          <h4>Missão</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+            reiciendis ipsam eius vel, temporibus nobis assumenda corporis
+            optio, ratione debitis quia, culpa sunt? Excepturi iste rerum hic
+            natus perspiciatis cupiditate?
+          </p>
+        </div>
+        <div>
+          <h4>Visão</h4>
+          <p>
+            Quas nostrum repellendus, autem corporis expedita debitis ea eveniet
+            consectetur? Nesciunt voluptatum in est reiciendis blanditiis nam
+            totam dolorem et ratione labore. Suscipit maxime libero placeat
+            adipisci eveniet. Laborum, perspiciatis!
+          </p>
+        </div>
+      </Grid>
+    </Section>
+    <Section>
+      <Heading>
+        <h2>Conheça nossos professores</h2>
+      </Heading>
+      <Grid sm={2} md={3} lg={4}>
+        {instructors.map((instructor) => (
+          <Card key={instructor.id}>
+            <CardMedia image={instructor.avatar}>
+              <CardMediaDescription>
+                <h5>{instructor.name}</h5>
+              </CardMediaDescription>
+            </CardMedia>
+          </Card>
+        ))}
+      </Grid>
+    </Section>
+    <Footer />
+  </>
+);
 
 export default About;
--- src/components/pages/ProductDetail.js
+++ src/components/pages/ProductDetail.js
@@ -3,8 +3,6 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FaIdCard, FaHome, FaScroll } from "react-icons/fa";
 
-import { useScrollToTop } from "hooks/scroll";
-
 import Hero from "components/molecules/Hero";
 import Heading from "components/atoms/Heading";
 import Section from "components/molecules/Section";
@@ -38,85 +36,81 @@ const PinnedItem = styled.li`
   }
 `;
 
-const ProductDetail = () => {
-  useScrollToTop();
-
-  return (
-    <>
-      <Hero image={HeroImage}>
-        <Heading>
-          <h1>Nome do Serviço</h1>
-        </Heading>
-        <BreadCrumb
-          items={[
-            { label: "Início", link: "/" },
-            { label: "Serviços" },
-            { label: "Nome do Serviços" },
-          ]}
-        />
-      </Hero>
-      <Section>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero dolor
-          perferendis distinctio. Praesentium veniam at voluptate suscipit,
-          earum voluptates adipisci reprehenderit delectus magni, qui tenetur
-          atque temporibus aliquid, esse voluptatem.
-        </p>
-        <p>
-          Incidunt totam modi veniam provident optio repudiandae illo odio,
-          numquam inventore, deserunt voluptas sed quam minima delectus dolore
-          veritatis iusto, voluptatibus neque consequatur. Nemo eum, maxime
-          ipsam magnam fugit commodi!
-        </p>
-        <p>
-          Illum corporis voluptas laboriosam molestias tenetur rerum vel autem
-          ipsam adipisci temporibus, odio fuga neque at unde ab similique
-          quaerat deserunt iure! Eum nesciunt recusandae, distinctio et pariatur
-          assumenda facere!
-        </p>
-        <h5>Documentos necessários:</h5>
-        <PinnedList>
-          <PinnedItem>
-            <FaIdCard />
-            RG
-          </PinnedItem>
-          <PinnedItem>
-            <FaIdCard />
-            CPF
-          </PinnedItem>
-          <PinnedItem>
-            <FaScroll />
-            Certidão de nascimento ou casamento
-          </PinnedItem>
-          <PinnedItem>
-            <FaHome />
-            Comprovante de residência
-          </PinnedItem>
-        </PinnedList>
-      </Section>
-      <Section inverse>
-        <Callout>
-          <CalloutBody>
-            <h6>Faça a sua matrícula agora mesmo</h6>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
-              nobis, dolores, tempore veritatis iusto nisi eaque minus earum
-              nostrum inventore doloribus? Autem repellendus eligendi delectus
-              ipsam quae repellat quas libero?
-            </p>
-            <CalloutActions>
-              <Button color="primary">Matricular</Button>
-            </CalloutActions>
-          </CalloutBody>
-          <CalloutMedia>
-            <SpeedImage />
-          </CalloutMedia>
-        </Callout>
-      </Section>
-      <Footer />
-    </>
-  );
-};
+const ProductDetail = () => (
+  <>
+    <Hero image={HeroImage}>
+      <Heading>
+        <h1>Nome do Serviço</h1>
+      </Heading>
+      <BreadCrumb
+        items={[
+          { label: "Início", link: "/" },
+          { label: "Serviços" },
+          { label: "Nome do Serviços" },
+        ]}
+      />
+    </Hero>
+    <Section>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero dolor
+        perferendis distinctio. Praesentium veniam at voluptate suscipit, earum
+        voluptates adipisci reprehenderit delectus magni, qui tenetur atque
+        temporibus aliquid, esse voluptatem.
+      </p>
+      <p>
+        Incidunt totam modi veniam provident optio repudiandae illo odio,
+        numquam inventore, deserunt voluptas sed quam minima delectus dolore
+        veritatis iusto, voluptatibus neque consequatur. Nemo eum, maxime ipsam
+        magnam fugit commodi!
+      </p>
+      <p>
+        Illum corporis voluptas laboriosam molestias tenetur rerum vel autem
+        ipsam adipisci temporibus, odio fuga neque at unde ab similique quaerat
+        deserunt iure! Eum nesciunt recusandae, distinctio et pariatur assumenda
+        facere!
+      </p>
+      <h5>Documentos necessários:</h5>
+      <PinnedList>
+        <PinnedItem>
+          <FaIdCard />
+          RG
+        </PinnedItem>
+        <PinnedItem>
+          <FaIdCard />
+          CPF
+        </PinnedItem>
+        <PinnedItem>
+          <FaScroll />
+          Certidão de nascimento ou casamento
+        </PinnedItem>
+        <PinnedItem>
+          <FaHome />
+          Comprovante de residência
+        </PinnedItem>
+      </PinnedList>
+    </Section>
+    <Section inverse>
+      <Callout>
+        <CalloutBody>
+          <h6>Faça a sua matrícula agora mesmo</h6>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+            nobis, dolores, tempore veritatis iusto nisi eaque minus earum
+            nostrum inventore doloribus? Autem repellendus eligendi delectus
+            ipsam quae repellat quas libero?
+          </p>
+          <CalloutActions>
+            <Button color="primary">Matricular</Button>
+          </CalloutActions>
+        </CalloutBody>
+        <CalloutMedia>
+          <SpeedImage />
+        </CalloutMedia>
+      </Callout>
+    </Section>
+    <Footer />
+  </>
+);
 
 ProductDetail.defaultProps = {};
 

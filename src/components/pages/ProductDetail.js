fab397edd45f1e3f48339f0e287e2b070ecad5c7
jnakagawa1 <jeannak@gmail.com>
2021-03-20 21:19:10 -0300

    Create product hook

--- src/components/pages/ProductDetail.js
+++ src/components/pages/ProductDetail.js
@@ -1,7 +1,8 @@
 import React from "react";
-import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FaIdCard, FaHome, FaScroll } from "react-icons/fa";
+
+import ProductType from "types/ProductType";
 
 import Hero from "components/molecules/Hero";
 import Heading from "components/atoms/Heading";
@@ -36,17 +37,17 @@ const PinnedItem = styled.li`
   }
 `;
 
-const ProductDetail = () => (
+const ProductDetail = ({ product }) => (
   <>
     <Hero image={HeroImage}>
       <Heading>
-        <h1>Nome do Serviço</h1>
+        <h1>{product.title}</h1>
       </Heading>
       <BreadCrumb
         items={[
           { label: "Início", link: "/" },
           { label: "Serviços" },
-          { label: "Nome do Serviços" },
+          { label: product.title },
         ]}
       />
     </Hero>
@@ -112,8 +113,12 @@ const ProductDetail = () => (
   </>
 );
 
-ProductDetail.defaultProps = {};
+ProductDetail.defaultProps = {
+  product: {},
+};
 
-ProductDetail.propTypes = {};
+ProductDetail.propTypes = {
+  product: ProductType,
+};
 
 export default ProductDetail;
